3c094c9dbbb4f03f9244f9a1251bb057d4ec8054
patrice <patrice>
2006-08-20 19:05:43 +0000

    Do not activate audio at start

--- src/i_system.c
+++ src/i_system.c
@@ -114,7 +114,7 @@ void I_Update (void)
 
 void I_Init (void)
 {
-	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_JOYSTICK|SDL_INIT_AUDIO)<0) {
+	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_JOYSTICK)<0) {
 		fprintf(stderr, "Can not initialize SDL: %s\n", SDL_GetError());
 		exit(1);
 	}
